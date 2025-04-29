dbeba4a2be7b6f0d9e7b2a72ca94944ae4ba6ccc
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-04-28 23:28:19 -0230

    update main.js

--- js/main.js
+++ js/main.js
@@ -18,3 +18,66 @@ import {
   gameOver
 } from './UI.js';
 
+// Create Scene
+const scene = new THREE.Scene();
+
+// Variables for camera
+const fov = 60;
+const aspect = window.innerWidth / window.innerHeight;
+const near = 1.0;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+const renderer = new THREE.WebGLRenderer();
+
+// Create clock
+const clock = new THREE.Clock();
+
+// List of models to load
+const files = [
+  { name: 'spaceship', url: '/models/spaceship.glb' },
+  { name: 'blob', url: '/models/blob.glb' }
+];
+
+// Load resources
+const resources = new Resources(files);
+await resources.loadAll();
+
+// Create player
+const player = new Character(0x0000ff);
+player.setModel(resources.get('spaceship'));
+
+let playerLives = 5;
+let isGameOver = false;
+let invincibilityFrames = 0; // To prevent multiple hits at once
+
+// Create asteroids
+const asteroids = [];
+const asteroidCount = 500; // Number of asteroids to create
+for (let i = 0; i < asteroidCount; i++) {
+  const asteroid = new Asteroid();
+  asteroids.push(asteroid.gameObject);
+  scene.add(asteroid.gameObject);
+}
+
+// Create aliens array
+const aliens = [];
+const alienCount = 2; // Number of aliens to create
+
+// Declare bounds
+let bounds;
+
+// Declare forward guidance line
+let forwardLine;
+
+// Declare variables for overdrive and shooting, also array to hold lasers
+let overdrive = false;
+let shooting = false;
+let laserBeams = [];
+
+
+let fuelLevel = 100; // Full tank at start
+let fuelGaugeHeight = 200; // Height of the visual fuel gauge
+let fuelGaugeWidth = 30; // Width of the fuel gauge
+let isFuelDepleted = false;
+
