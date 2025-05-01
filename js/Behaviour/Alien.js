f4fe0b0e6dee609cf6472c2daf8aa0dc8fdad949
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-04-30 23:55:11 -0230

    update alien.js

---
+++ js/Behaviour/Alien.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+import { VectorUtil } from '../Util/VectorUtil.js';
+import { Character } from './Character.js';
+import { WanderState, PursueState } from './AlienState.js';
+
