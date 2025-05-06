857f2dc76f844f1f7783936cfe6318d2b036624e
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-05 23:39:55 -0230

    ThirdPersonCamera constructor

--- js/Behaviour/ThirdPersonCamera.js
+++ js/Behaviour/ThirdPersonCamera.js
@@ -1,5 +1,14 @@
 import * as THREE from 'three';
 
 export class ThirdPersonCamera {
-  
+  constructor(camera, target) {
+      this.camera = camera;
+      this.target = target;
+      
+      // Camera offset from player (in player's local space)
+      this.offset = new THREE.Vector3(0, 8, -12); // Above and behind
+      
+      this.lagFactor = 0.1;
+    }
+
 }
