fd90e8b75d41c0f6b0d234c838e09060ef46b832
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-05 23:40:20 -0230

    ThirdPersonCamera update function

--- js/Behaviour/ThirdPersonCamera.js
+++ js/Behaviour/ThirdPersonCamera.js
@@ -9,6 +9,30 @@ export class ThirdPersonCamera {
       this.offset = new THREE.Vector3(0, 8, -12); // Above and behind
       
       this.lagFactor = 0.1;
-    }
+  }
 
+    
+  update() {
+    if (!this.target) return;
+
+    // Clone the offset and rotate it by the character's quaternion (which includes pitch & yaw)
+    const offsetRotated = this.offset.clone();
+    offsetRotated.applyQuaternion(this.target.quaternion);
+
+    // Calc desired camera position: character position + rotated offset
+    const desiredPos = new THREE.Vector3()
+      .copy(this.target.position)
+      .add(offsetRotated);
+
+    // Smoothly lerp camera toward desired position
+    this.camera.position.lerp(desiredPos, this.lagFactor);
+
+    // Make the camera look slightly ahead of the character
+    const lookAhead = this.target.position.clone();
+    const forwardOffset = new THREE.Vector3(0, 0, 10); 
+    forwardOffset.applyQuaternion(this.target.quaternion);
+    lookAhead.add(forwardOffset);
+
+    this.camera.lookAt(lookAhead);
+  }
 }
