2a0614db2fb7c3f1e1c884357e2e99b500e998c2
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-08 23:57:12 -0230

    util vectorutil

---
+++ js/Util/VectorUtil.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+
+export class VectorUtil {
+
+	// Add two vectors
+	// Returns a new vector = a + b
+	static add(a, b) {
+		let v = a.clone();
+		v.add(b);
+		return v;
+	}
+
+	
+
+}
