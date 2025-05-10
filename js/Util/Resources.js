5cf0600fd0635ea076720fbfb2042cc7217e74f8
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-09 23:44:21 -0230

    util resources cont'd

--- js/Util/Resources.js
+++ js/Util/Resources.js
@@ -2,5 +2,11 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export class Resources {
+  constructor(fileList) {
+    this.loader = new GLTFLoader();
+    this.files = fileList; // array of { name, url }
+    this.models = new Map(); // stores loaded models
+  }
+
   
 }
