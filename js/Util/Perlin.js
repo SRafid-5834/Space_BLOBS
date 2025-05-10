c27ac31afd38ed03f22d36a418ede840d0e7e766
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-09 23:45:49 -0230

    util perlin cont'd

--- js/Util/Perlin.js
+++ js/Util/Perlin.js
@@ -2,5 +2,11 @@ import * as THREE from 'three';
 import { MathUtil } from '../Util/MathUtil.js';
 
 export class Perlin {
+  constructor(size) {
+    this.size = size;
+    this.gradients = [];
+    this.createGrid();
+  }
+  
   
 }
