70b38c82a101ded773cc23c58922f8414bcbe0d5
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-09 23:47:00 -0230

    util perlin cont'd

--- js/Util/Perlin.js
+++ js/Util/Perlin.js
@@ -44,5 +44,26 @@ export class Perlin {
     let xoffset = x - x0;
     let yoffset = y - y0;
     
+    let dx0y0 = new THREE.Vector2(xoffset, yoffset);
+    let dx1y0 = new THREE.Vector2(xoffset - 1, yoffset);
+    let dx0y1 = new THREE.Vector2(xoffset, yoffset - 1);
+    let dx1y1 = new THREE.Vector2(xoffset - 1, yoffset - 1);
+    
+    // These lines were failing - ensure indices are valid
+    let dot1 = this.gradients[x0][y0].dot(dx0y0);
+    let dot2 = this.gradients[x1][y0].dot(dx1y0);
+    let dot3 = this.gradients[x0][y1].dot(dx0y1);
+    let dot4 = this.gradients[x1][y1].dot(dx1y1);
+    
+    let fadedXOffset = this.fade(xoffset);
+    let fadedYOffset = this.fade(yoffset);
+    
+    let lerp1 = MathUtil.lerp(dot1, dot2, fadedXOffset);
+    let lerp2 = MathUtil.lerp(dot3, dot4, fadedXOffset);
+    let average = MathUtil.lerp(lerp1, lerp2, fadedYOffset);
+    
+    return MathUtil.map(average, -1, 1, 0, 1);
   }
+  
+  
 }
