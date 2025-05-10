3c4f9e3d9d3b67f3df5b3e5a7192276769406c67
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-09 23:46:09 -0230

    util perlin cont'd

--- js/Util/Perlin.js
+++ js/Util/Perlin.js
@@ -8,5 +8,26 @@ export class Perlin {
     this.createGrid();
   }
   
+  // Populates our gradient grid 
+  // with random vectors
+  createGrid() {
+    for (let i = 0; i < this.size; i++) {
+      let row = [];
+
+      for (let j = 0; j < this.size; j++) {
+        // Get a random x and y
+        let x = MathUtil.map(Math.random(), 0, 1, -1, 1);
+        let y = MathUtil.map(Math.random(), 0, 1, -1, 1);
+ 
+        let gradient = new THREE.Vector2(x, y);
+        // normalize the gradient length
+        gradient.setLength(1);
+        row.push(gradient);
+      }
+      // Push to our gradient grid
+      this.gradients.push(row);
+    }
+  }
+  
   
 }
