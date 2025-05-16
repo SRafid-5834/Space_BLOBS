14667fc861c74fc57dfde31e73d9de0b45efc372
Saadman Rafid <157035365+SRafid-5834@users.noreply.github.com>
2025-05-15 23:40:43 -0230

    3d map graph cont'd

--- js/World/MapGraph3D.js
+++ js/World/MapGraph3D.js
@@ -12,5 +12,23 @@ export class MapGraph3D {
     this.connectNodes();
   }
 
+  // Create grid of nodes based on bounds and cell size
+  createGrid() {
+    const { min, max } = this.bounds;
+    let id = 0;
+    
+    for (let x = min.x; x <= max.x; x += this.cellSize) {
+      for (let y = min.y; y <= max.y; y += this.cellSize) {
+        for (let z = min.z; z <= max.z; z += this.cellSize) {
+          const node = new MapNode3D(id++, x, y, z);
+          this.nodes.push(node);
+          // Store node in map for quick lookup
+          const key = `${x},${y},${z}`;
+          this.nodeMap.set(key, node);
+        }
+      }
+    }
+  }
+
   
 }
